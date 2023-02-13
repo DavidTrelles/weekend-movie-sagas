738d1f3a9491da1a8052dcf4e02cbf44e8c3bc05
David Trelles <davidstrelles@gmail.com>
2023-02-13 11:06:50 -0600

    now displaying movie number in movie details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux";
+
 const MovieDetails = () => {
+  const movie = useSelector((store) => store.movie);
   return (
     <>
-      <p>CUrrently under construction</p>
+      <p>CUrrently under construction, this is your Movie: {movie}</p>
     </>
   );
 };
