e8c4b8019531a51bf1afe23dd29cc78654c0bdc9
David Trelles <davidstrelles@gmail.com>
2023-02-18 09:59:13 -0600

    added a genres router with a join, started attempting changes to movieList CSS

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  res.sendStatus(500);
+});
+
+router.get("/:id", (req, res) => {
+  console.log("hello from id request!", req.params.id);
+  const queryText = `SELECT "genres"."name" FROM "movies"
+JOIN "movies_genres" ON "movies_genres"."movie_id" = "movies"."id"
+JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
+WHERE "movie_id" = $1`;
+  let id = req.params.id;
+  pool
+    .query(queryText, [id])
+    .then((result) => {
+      console.log(result);
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Getting the movie", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -3,11 +3,6 @@ import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 const MovieDetails = () => {
   const history = useHistory();
-  // const dispatch = useDispatch();
-  // const movieId = useSelector((store) => store.movieId);
-  // useEffect(() => {
-  //   dispatch({ type: "FETCH_MOVIE", payload: movieId });
-  // }, []);
   const movie = useSelector((store) => store.movie);
   console.log(movie);
   const handleClick = () => {
@@ -17,10 +12,6 @@ const MovieDetails = () => {
     <>
       {movie.length > 0 && (
         <>
-          {/* <div>{JSON.stringify(movie)}</div> */}
-          {/* <p>
-            CUrrently under construction, this is your Movie: {movie[0].title}
-          </p> */}
           <h3>{movie[0].title}</h3>
           <img src={movie[0].poster} alt={movie[0].title} />
           <p>{movie[0].description}</p>
